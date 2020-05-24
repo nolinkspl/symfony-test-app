87f024add306b39c7636df6c39787a9d7d962ea8
ars42 <arsnag03@gmail.com>
2020-05-24 22:07:37 +0300

    ExchangerController test token way

--- src/Controller/ExchangerController.php
+++ src/Controller/ExchangerController.php
@@ -9,16 +9,13 @@ class ExchangerController extends AbstractController
 
     public function index()
     {
-        var_dump('test1');
-
         return $this->render('base.html.twig');
     }
 
     public function token()
     {
-        var_dump('test2');
         $result = [1,2,3];
 
-        return json_encode($result);
+        return $this->json($result);
     }
 }
