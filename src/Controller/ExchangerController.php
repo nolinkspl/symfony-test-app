4d271bace5fd4f524c5bcf4182b9d68af3767256
ars42 <arsnag03@gmail.com>
2020-05-24 19:22:12 +0300

    ExchangerController

---
+++ src/Controller/ExchangerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ExchangerController extends AbstractController
+{
+
+    public function index()
+    {
+        var_dump('test1');
+    }
+
+    public function token()
+    {
+        var_dump('test2');
+    }
+}
