b25c1bfe6cf63e1f3d0c5a8f662b95d41d09b809
ars42 <arsnag03@gmail.com>
2020-05-25 19:19:37 +0300

    unitTest package added

---
+++ tests/Entity/ConversionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Amount;
+use App\Entity\Conversion;
+use PHPUnit\Framework\TestCase;
+
+class ConversionTest extends TestCase
+{
+
+    public function testExecute()
+    {
+
+    }
+}
---
+++ tests/Service/ConversionManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class ConversionManagerTest extends TestCase
+{
+
+    public function testFindAllConversionIds()
+    {
+
+    }
+
+    public function testFindConversionById()
+    {
+
+    }
+}
