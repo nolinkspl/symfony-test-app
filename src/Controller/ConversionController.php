14f365c3a999c22e8417ba5273910904aa75a456
ars42 <arsnag03@gmail.com>
2020-05-25 14:32:52 +0300

    conversion

--- src/Controller/ConversionController.php
+++ src/Controller/ConversionController.php
@@ -16,13 +16,6 @@ class ConversionController extends DefaultController
 
     public function conversion(int $id):JsonResponse
     {
-        /** @var Amount $amount */
-        $amount = $this->getDoctrine()
-                       ->getRepository(Amount::class)
-                       ->find($id);
-
-        var_dump($amount->currency()->getId());
-
         /** @var Conversion $result */
         $result = $this->getDoctrine()
                        ->getRepository(Conversion::class)
@@ -33,8 +26,6 @@ class ConversionController extends DefaultController
                 'No product found for id '.$id
             );
         }
-
-        var_dump($result->amount()->getId());
 
         return $this->json($result->info());
     }
