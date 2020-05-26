c8ec8d92a00ac897bb19168e10491fa9b14ca9e7
ars42 <arsnag03@gmail.com>
2020-05-26 17:36:01 +0300

    change amount to float

---
+++ src/Migrations/Version20200526143510.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200526143510 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE amount ALTER amount TYPE DOUBLE PRECISION');
+        $this->addSql('ALTER TABLE amount ALTER amount DROP DEFAULT');
+        $this->addSql('ALTER TABLE amount ALTER is_active DROP DEFAULT');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE amount ALTER amount TYPE BIGINT');
+        $this->addSql('ALTER TABLE amount ALTER amount DROP DEFAULT');
+        $this->addSql('ALTER TABLE amount ALTER is_active SET DEFAULT \'false\'');
+    }
+}
