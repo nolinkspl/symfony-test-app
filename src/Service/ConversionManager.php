a55eb9119ad90c1af4746714e15a60fa06b5bb62
ars42 <arsnag03@gmail.com>
2020-05-26 17:20:21 +0300

    debug

--- src/Service/ConversionManager.php
+++ src/Service/ConversionManager.php
@@ -105,7 +105,7 @@ class ConversionManager
         $result->setFromAmount($amount);
         $result->setToAmount((new Amount())->setCurrency($toCurrency));
 var_dump($fromCurrency->getRates());
-        $rate = $this->propertyAccessor->getValue($fromCurrency->getRates(), $toCurrency->getCode());
+        $rate = $this->propertyAccessor->getValue($fromCurrency->getRates(), "[{$toCurrency->getCode()}]");
 
         if (empty($rate)) {
             throw new HttpException(406, 'Rate doesnt exists');
